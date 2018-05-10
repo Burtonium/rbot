f9035d1c6a8c1e07d989de0b3fbe4436f280b6af
Eric Roberts <ericjasonroberts@gmail.com>
2018-05-10 21:07:13 +0000

    user can add and remove reference and trigger markets

--- src/components/OrderCaddyView.vue
+++ src/components/OrderCaddyView.vue
@@ -29,9 +29,44 @@
           <td>{{ +market.tickers[0].bid }}</td>
           <td>{{ +market.tickers[0].ask }}</td>
           <td>{{ dateTimeString(market.tickers[0].timestamp) }}</td>
+          <button @click="removeReferenceMarket(index)">-</button>
         </tr>
       </tbody>
     </table>
+    <div class="form-group form-inline">
+      <v-select :options="referenceMarketOptions" v-model="referenceMarket"/>
+    </div>
+    <br>
+    <h4>Trigger Markets</h4>
+    <table class="table table-striped table-bordered orders">
+      <thead>
+        <tr>
+          <th>Exchange</th>
+          <th>Side</th>
+          <th>Amount</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(market, index) in triggerMarkets" :key="index">
+          <td>{{ market.exchange.name }}</td>
+          <td>{{ market.side }}</td>
+          <td>{{ market.amount }}</td>
+          <button @click="removeTriggerMarket(index)">-</button>
+        </tr>
+      </tbody>
+    </table>
+    <div class="form-group form-inline">
+      <v-select :options="triggerMarketOptions" v-model="triggerMarket"/>
+      <div v-if="triggerMarket">
+        <select v-model="side" class="form-control">
+          <option value="buy">Buy</option>
+          <option value="sell">Sell</option>
+        </select>
+        <span v-if="side" class="input-group">
+          <input class="form-control" v-model.lazy="amount" type="number"/>
+        </span>
+      </div>
+    </div>
     <br>
     <h4>Open Triggers</h4>
     <table class="table table-striped table-bordered orders">
@@ -83,15 +118,30 @@
   </div>
 </template>
 <script>
-import { fetchOrderCaddy, patchCaddy } from '@/api';
+import { fetchOrderCaddy, patchCaddy, fetchMarkets } from '@/api';
 import { capitalize } from 'lodash';
 import { dateTimeString } from '@/utils';
 
 export default {
   data() {
     return {
-      caddy: null
+      caddy: null,
+      exchanges: [],
+      allMarkets: [],
+      referenceMarket: null,
+      triggerMarket: null,
+      side: '',
+      amount: 0,
+      allMarkets: []
     };
+  },
+  watch: {
+    referenceMarket() {
+      this.addReferenceMarket();
+    },
+    amount() {
+      this.addTriggerMarket();
+    }
   },
   computed: {
     openTriggers() {
@@ -100,23 +150,78 @@ export default {
     referenceMarkets() {
       return this.caddy ? this.caddy.referenceMarkets : [];
     },
+    triggerMarkets() {
+      return this.caddy ? this.caddy.triggerMarkets : [];
+    },
     arbs() {
       return this.caddy ? this.caddy.triggers.filter(t => t.arbCycle).map(t => t.arbCycle) : [];
+    },
+    marketOptions() {
+      return this.markets.map(m => ({ label: m.exchange.name, value: m.id }));
+    },
+    markets() {
+      return this.allMarkets.filter(m => m.pair.id === this.caddy.pair.id, 10);
+    },
+    referenceMarketOptions() {
+      return this.marketOptions.filter(m =>
+        !this.caddy.referenceMarkets.find(rm => m.value === rm.id));
+    },
+    triggerMarketOptions() {
+      return this.marketOptions;
     }
   },
   methods: {
     capitalize,
     dateTimeString,
     async update() {
+      if (!(this.caddy.label.length
+          && this.caddy.referenceMarkets.length
+          && this.caddy.triggerMarkets.length)) {
+        this.caddy.active = false;
+      }
       this.caddy = (await patchCaddy(this.caddy)).caddy;
     },
-    toggleActive() {
+    async toggleActive() {
       this.caddy.active = !this.caddy.active;
+      await this.update();
+    },
+    async addReferenceMarket(){
+      const market = this.markets.find(m => m.id === this.referenceMarket.value);
+      if (market) {
+        market.tickers = [{
+          ask: 0,
+          bid: 0
+        }];
+        
+        if (!this.caddy.referenceMarkets.find(m => m.id === market.id)) {
+          this.caddy.referenceMarkets.push(market);
+          await this.update();
+        }
+      }
+    },
+    async addTriggerMarket(){
+      const market = this.markets.find(m => m.id === this.triggerMarket.value);
+      this.caddy.triggerMarkets.push({
+        side: this.side,
+        amount: this.amount,
+        id: market.id
+      });
+      await this.update();
+      this.triggerMarket = null;
+      this.amount = 0;
+    },
+    removeReferenceMarket(index) {
+      this.caddy.referenceMarkets.splice(index, 1);
+      this.update();
+    },
+    removeTriggerMarket(index) {
+      this.caddy.triggerMarkets.splice(index, 1);
       this.update();
     }
   },
   async mounted() {
     this.caddy = await fetchOrderCaddy(this.$route.params.id);
+    this.allMarkets = await fetchMarkets();
   }
 };
 </script>
